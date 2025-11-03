bd1e54d6ce5f3ec9cf50c374af67030e003dbdf0
Sandro Martínez <virgosmp.mx@gmail.com>
2025-11-02 22:42:37 -0600

    d

--- React Pojects/demo/src/App.jsx
+++ React Pojects/demo/src/App.jsx
@@ -29,6 +29,27 @@ const content = [
   ]
 ];
 
+/** 
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+  );
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Científicos increíbles</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
+*/
 export default function App() {
   const [activeContentIndex, setActiveContentIndex] = useState(0);
 
